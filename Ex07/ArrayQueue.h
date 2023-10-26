15a1d2049a345841b4ef49e691d98c107a3684ac
miyahaya <108918477+miyahaya@users.noreply.github.com>
2023-10-26 01:13:47 +0000

    first_commit

---
+++ Ex07/ArrayQueue.h
@@ -0,0 +1,139 @@
+// ArrayQueue.h
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+#ifndef ARRAY_QUEUE_H
+#define ARRAY_QUEUE_H
+
+class ArrayQueue {
+private:
+    int _num_items; // number of items in the queue
+    int _allocated_size; // size of memory allocated
+    int _first; // index to the first element of the queue 
+    int _last; // index of the next available slot 
+    std::string* _items; // container 
+
+    void resize(int max_size) {
+        if (max_size == 0) max_size++;
+
+        // Move stack to a new array of size max
+        std::string* temp = new std::string[max_size];
+        // Copy 
+        for (int i = 0; i < _num_items; ++i) {
+            temp[i] = _items[(i + _first) % _allocated_size];
+        }
+        delete[] _items;
+        _items = temp;
+
+        _first = 0;
+        _last = _num_items;
+        _allocated_size = max_size;
+    }
+
+public:
+    // Constructors:
+    // Default constructor 
+    ArrayQueue() : _num_items(0), _allocated_size(1), _first(0), _last(0), _items(new std::string[1]) {}
+
+    explicit ArrayQueue(int allocated_size) : _num_items(0), _allocated_size(allocated_size), _first(0), _last(0), _items(new std::string[allocated_size]) {}
+
+    // Copy constructor
+    ArrayQueue(const ArrayQueue& other) : _num_items(other._num_items), _allocated_size(other._allocated_size), _first(other._first), _last(other._last), _items(new std::string[other._allocated_size]) {
+        for (int i = 0; i < _num_items; ++i) {
+            _items[i] = other._items[(i + _first) % _allocated_size];
+        }
+    }
+
+    // Move constructor
+    ArrayQueue(ArrayQueue&& other) noexcept : _num_items(other._num_items), _allocated_size(other._allocated_size), _first(other._first), _last(other._last), _items(other._items) {
+        other._num_items = 0;
+        other._allocated_size = 1;
+        other._first = 0;
+        other._last = 0;
+        other._items = new std::string[1];
+    }
+
+    // Destructor:
+    ~ArrayQueue() {
+        delete[] _items;
+    }
+
+    // Assignment operator
+    ArrayQueue& operator=(const ArrayQueue& other) {
+        if (this != &other) {
+            // Delete existing data
+            delete[] _items;
+
+            // Copy data from other
+            _num_items = other._num_items;
+            _allocated_size = other._allocated_size;
+            _first = other._first;
+            _last = other._last;
+            _items = new std::string[other._allocated_size];
+            for (int i = 0; i < _num_items; ++i) {
+                _items[i] = other._items[(i + _first) % _allocated_size];
+            }
+        }
+        return *this;
+    }
+
+    // Move assignment operator
+    ArrayQueue& operator=(ArrayQueue&& other) noexcept {
+        if (this != &other) {
+            // Delete existing data
+            delete[] _items;
+
+            // Move data from other
+            _num_items = other._num_items;
+            _allocated_size = other._allocated_size;
+            _first = other._first;
+            _last = other._last;
+            _items = other._items;
+
+            // Reset other's data
+            other._num_items = 0;
+            other._allocated_size = 1;
+            other._first = 0;
+            other._last = 0;
+            other._items = new std::string[1];
+        }
+        return *this;
+    }
+
+    // Add an item to the queue 
+    void enqueue(const std::string& item) {
+        if (_num_items == _allocated_size) resize(2 * _allocated_size);
+        _items[_last++] = item;
+        if (_last == _allocated_size) _last = 0; // wrap 
+        _num_items++;
+    }
+
+    // Remove the item that was least recently added 
+    void dequeue() {
+        if (_num_items == 0) {
+            throw std::runtime_error("Queue is empty");
+        }
+        _num_items--;
+        _first++;
+        if (_first == _allocated_size) _first = 0; // wrap 
+        if (_num_items > 0 && _num_items == _allocated_size / 4) resize(_allocated_size / 2);
+    }
+
+    // Access the top-most item 
+    std::string peek() const {
+        if (_num_items == 0) {
+            throw std::runtime_error("Queue is empty");
+        }
+        return _items[_first];
+    }
+
+    // Check if the stack is empty 
+    bool empty() const { return _num_items == 0; }
+
+    // Return the number of elements in the stack 
+    int size() const { return _num_items; }
+};
+
+#endif // ARRAY_QUEUE_H
---
+++ Ex07/test_ArrayQueue.cpp
@@ -0,0 +1,36 @@
+#include "ArrayQueue.h"
+#include <iostream>
+
+int main(void) {
+    // Testing code from questions 1 and 2
+    try{
+    // Complete: Test the move operators and your implementation of the move semantic.
+    ArrayQueue queue1;
+    queue1.enqueue("Item 1");
+    queue1.enqueue("Item 2");
+    queue1.enqueue("Item 3");
+
+    ArrayQueue queue2 = std::move(queue1); // Move constructor
+    std::cout << "Queue 2 size: " << queue2.size() << std::endl;
+    std::cout << "Queue 2 peek: " << queue2.peek() << std::endl;
+
+    ArrayQueue queue3;
+    queue3.enqueue("Item A");
+    queue3.enqueue("Item B");
+
+    queue3 = std::move(queue2); // Move assignment operator
+    std::cout << "Queue 3 size: " << queue3.size() << std::endl;
+    std::cout << "Queue 3 peek: " << queue3.peek() << std::endl;
+    queue3.dequeue();
+    std::cout << "Queue 3 peek: " << queue3.peek() << std::endl;
+    queue3.dequeue();
+    std::cout << "Queue 3 peek: " << queue3.peek() << std::endl;
+    queue3.dequeue();
+    std::cout << "Queue 3 peek: " << queue3.peek() << std::endl;
+
+    } catch (const std::runtime_error& e) {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
